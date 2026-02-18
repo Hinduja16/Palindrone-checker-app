6036acf8bf6b2ed3507a53de966732be54e9727a
hinduja16 <hinduja1635@gmail.com>
2026-02-18 16:17:54 +0530

    hinduja added uc1

---
+++ App/src/PalindromeChecker.java
@@ -0,0 +1,15 @@
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        // Application Welcome Message
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome Checker");
+        System.out.println("   Version: 1.0");
+        System.out.println("=================================");
+
+        // Next use case will be called here
+        System.out.println("Application started successfully...");
+    }
+}
+
